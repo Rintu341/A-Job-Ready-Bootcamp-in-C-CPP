10fc1e250d6bb13f93ac806106b7507b17f3ac64
Rintu <sujanrintu@gmail.com>
2022-07-21 17:02:06 +0530

    Assignment 3  problem 17,18 update

--- Assignment3/problem17.c
+++ Assignment3/problem17.c
@@ -2,6 +2,13 @@
 #include <conio.h>
 
 int main(){
-    
+    int a,b,c;
+    printf("Enter three sides of triangle: ");
+    scanf("%d %d %d",&a,&b,&c);
+    if(a+b>c&&b+c>a&&a+c>b)
+        printf("Triangle is vaild");
+    else    
+        printf("Triangle is Not vaild");
+
     return 0;
 }
---
+++ Assignment3/problem18.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main(){
+    int M_number;
+    printf("Enter Mounth Number : ");
+    scanf("%d",&M_number);
+    if (M_number==1)
+        printf("31 days in this month");
+    else{
+        if (M_number==2)
+            printf("28 days in this month");
+            else{
+                if (M_number==3)
+                    printf("31 days in this month");
+                else{
+                    if (M_number==4)
+                        printf("30 days in this month");
+                    else{
+                        if (M_number==5)
+                            printf("31 days in this month");
+                        else{
+                            if (M_number==6)
+                                printf("30 days in this month");
+                            else{
+                                if (M_number==7)
+                                    printf("31 days in this month");
+                                else{
+                                    if (M_number==8)
+                                        printf("31 days in this month");
+                                    else{
+                                        if (M_number==9)
+                                            printf("30 days in this month");
+                                        else{
+                                            if (M_number==10)
+                                                printf("31 days in this month");
+                                            else{
+                                                if (M_number==11)
+                                                    printf("30 days in this month");
+                                                else{
+                                                    if (M_number==12)
+                                                        printf("31 days in this month");
+                                                }       
+                                            }    
+                                        }    
+                                    }
+                                }    
+                            }    
+                        }    
+                    }    
+                }
+         }
+    }
+    return 0;
+}
