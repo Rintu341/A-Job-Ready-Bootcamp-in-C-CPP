9fd607d65b4d901cc0927839465b42c51726987e
Rintu <sujanrintu@gmail.com>
2022-08-17 19:55:26 +0530

    first version

--- Project1/NumberPuzzleGame.c
+++ Project1/NumberPuzzleGame.c
@@ -7,7 +7,36 @@ void randgen(int a[][4]);
 void moveNumber(int a[][4]);
 int checksort(int a[][4]);
 void position(int a[][4], int *, int *);
-int hashfunction(int key, int hasharr[]) // hash function
+void red()
+{
+    printf("\033[1;31m");
+}
+void yellow()
+{
+    printf("\033[1;33m");
+}
+
+void reset()
+{
+    printf("\033[0m");
+}
+void Blue()
+{
+    printf("\033[1;34m");
+}
+void green()
+{
+    printf("\033[1;32m");
+}
+void cyan()
+{
+    printf("\033[1;36m");
+}
+void purple()
+{
+    printf("\033[1;35m");
+}
+int hashfunction(int key, int hasharr[]) // hash function it check unique value
 {
     if (hasharr[key] > 0)
         return 1;
@@ -17,24 +46,31 @@ int hashfunction(int key, int hasharr[]) // hash function
         return 0;
     }
 }
-void Rule(){
-    printf("\n                               MATRIX PUZZLE\n");
-    printf("                           A. RULE OF THIS GAME\n");
+void Rule()
+{
+    green();
+    printf("\n                               MATRIX PUZZLE");
+    Blue();
+    printf("\n                           A. RULE OF THIS GAME\n");
+    yellow();
     printf("        1.You can move only 1 step at a time by given key\n");
-    printf("            Move Up   : by 'u' key\n");
-    printf("            Move Down : by 'b' key\n");
-    printf("            Move Left : by 'f' key\n");
-    printf("            Move Right: by 'j' key\n");
+    printf("            Move Up   : by Up key\n");
+    printf("            Move Down : by Down key\n");
+    printf("            Move Left : by Left key\n");
+    printf("            Move Right: by Right key\n");
     printf("        2.You can move number at empty position only\n");
     printf("        3.For each valid move : your total number of move will Decreased by 1\n");
     printf("        4.Wining situation : Number in a 4*4 matrix should be in order from 1 to 15\n");
+    purple();
     printf("                ~ Wining situation ~\n");
+    cyan();
     printf("---------------------\n");
     printf("|  1 |  2 |  3 |  4 |\n");
     printf("|  5 |  6 |  7 |  8 |\n");
     printf("|  9 | 10 | 11 | 12 |\n");
     printf("| 13 | 14 | 15 |    |\n");
     printf("---------------------\n");
+    reset();
 }
 void display(int arr[][4])
 {
@@ -64,17 +100,21 @@ int main()
     Rule();
     while (1)
     {
+        green();
         printf("\nDo you want play this game(y/n)?: ");
         switch (getch())
         {
         case 'y':
-            // printf("\e[1;1H\e[2J");
+            printf("\e[1;1H\e[2J");
             // clrscr();
-            system("clear");
+            // system("clear");
             randgen(Arr);
+            Blue();  
             printf("\nPlayer Name: ");
             fgets(str, 30, stdin);
-            printf("Hello %s", str);
+            yellow();
+            printf("Let's Play %s", str);
+            printf("                ,move remaining : 1000\n");
             display(Arr);
             printf("\n");
             moveNumber(Arr);
@@ -115,75 +155,78 @@ void randgen(int a[][4])
 }
 void moveNumber(int a[][4])
 {
-
-    // printf("%d %d ",i ,j);
-    char b;
-    int move = 200;
+    int b;
+    int move = 1000;
     int i, j, temp;
-    printf("move remaining : %d\n", move);
+    green();
     while (1)
-    {   
+    {
         if (move == 0)
         {
-            printf("Better Luck Next Time\n");
+            red();
+            printf("\nBetter Luck Next Time\n");
             break;
         }
-        b = getch();
-        printf("Your press %c",b);
+        b = _getch();
+        printf("Your press %c", b);
         printf("\e[1;1H\e[2J");
-        if ((b == 'e' || b == 'E'))
-            break;
-        else
+        if (b == 0 || b == 224)
         {
-            position(a, &i, &j);
-            switch (b)
+            if ((b == 'e' || b == 'E'))
+                break;
+            else
             {
-            case 'u': // up
-                if ((i + 1) < 4)
+                position(a, &i, &j);
+                switch (_getch())
                 {
-                    temp = a[i + 1][j];
-                    a[i + 1][j] = a[i][j];
-                    a[i][j] = temp;
+                case 72: // up
+                    if ((i + 1) < 4)
+                    {
+                        temp = a[i + 1][j];
+                        a[i + 1][j] = a[i][j];
+                        a[i][j] = temp;
+                    }
+                    break;
+                case 80: // down
+                    if ((i - 1) >= 0)
+                    {
+                        temp = a[i - 1][j];
+                        a[i - 1][j] = a[i][j];
+                        a[i][j] = temp;
+                    }
+                    break;
+                case 75: // left
+                    if ((j + 1) < 4)
+                    {
+                        temp = a[i][j + 1];
+                        a[i][j + 1] = a[i][j];
+                        a[i][j] = temp;
+                    }
+                    break;
+                case 77: // right
+                    if ((j - 1) >= 0)
+                    {
+                        temp = a[i][j - 1];
+                        a[i][j - 1] = a[i][j];
+                        a[i][j] = temp;
+                    }
+                    break;
+                default:
+                    printf("Press Valid move!\n");
+                    move++;
+                    break;
                 }
-                break;
-            case 'b': // down
-                if ((i - 1) >= 0)
-                {
-                    temp = a[i - 1][j];
-                    a[i - 1][j] = a[i][j];
-                    a[i][j] = temp;
-                }
-                break;
-            case 'f': // left
-                if ((j + 1) < 4)
-                {
-                    temp = a[i][j + 1];
-                    a[i][j + 1] = a[i][j];
-                    a[i][j] = temp;
-                }
-                break;
-            case 'j': // right
-                if ((j - 1) >= 0)
-                {
-                    temp = a[i][j - 1];
-                    a[i][j - 1] = a[i][j];
-                    a[i][j] = temp;
-                }
-                break;
-            default:
-                printf("Press Valid move!\n");
-                move++;
-                break;
             }
             if (checksort(a))
             {
                 display(a);
+                green();
                 printf("You Win");
                 break;
             }
             display(a);
             move = move - 1;
-        printf("move remaining : %d\n", move);
+            printf("move remaining : %d\n", move);
         }
     }
 }
--- Project1/test.c
+++ Project1/test.c
@@ -1,32 +1,42 @@
 #include <stdio.h>
 #include <conio.h>
-#include <stdlib.h>
+int checksort(int a[][4]);
+int main ()
+{   
+    // int ch;
+    // printf("\033[34m This is yellow\033[0m");
+    // while ((ch = _getch()) != 27) /* 27 = Esc key */
+    // {
+    //     printf("%d", ch);
+    //     if (ch == 0 || ch == 224)
+    //         printf (", %d", _getch ()); 
+    //     printf("\n");
+    // }
 
-
-int main()
+    // printf("ESC %d\n", ch);
+    int a[4][4]={{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
+    if(checksort(a))
+        printf("Sorted");
+    else
+        printf("Unsort");
+    return (0);
+}
+int checksort(int a[][4])
 {
-	unsigned char a;
-	while(1)
-	{		
-		system("cls");
-		a=getch();
-		//for detect the function\arrow keys 
-		//we must call the getch() again
-		//testing if a is '0' or '0xE0'
-		if (a==0 || a==0xE0) a=getch();
-		
-		if (a==27) //ESC for exit the 'while'
-			break;
-		else if (a==72)
-			printf("UP");
-		else if (a==80)
-			printf("DOWN");
-		else if (a==75)
-			printf("LEFT");
-		else if (a==77)
-			printf("RIGHT");		
-		else
-			printf("%d",(int) a);	
-	}
-	return 0;
+    int i, j, temp = a[0][0], pre;
+    for (i = 0; i < 4; i++)
+    {
+        for (j = 0; j < 4; j++)
+        {
+            pre = a[i][j + 1];
+            if (temp > pre)
+            {
+                return 0;
+            }
+            printf("temp =%d,pre =%d\n",temp,pre);
+            temp = pre;
+
+        }
+    }
+    return 1;
 }
