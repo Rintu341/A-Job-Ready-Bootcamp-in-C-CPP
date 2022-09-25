3650ec660623f87a89171b7c026606ccc8d5a689
Rintu <sujanrintu@gmail.com>
2022-09-25 17:20:03 +0530

    First CP

---
+++ Competitive_Programming/palindrome.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+class Solution {
+public:
+    bool isPalindrome(int x) {
+            int y=x,rev=0;
+        if(0>x)
+            return false;
+        else{
+            while(x!=0){
+                rev=rev*10+x%10;
+                x=x/10;
+            }
+        }
+            if(y==rev)
+                    return true;
+            else
+                    return false;
+    }
+};
+int main(){
+    Solution s1;
+    if(s1.isPalindrome(1000000001))
+        cout<<"Hello";
+    else
+        cout<<"gandu";
+
+    return 0;
+}
